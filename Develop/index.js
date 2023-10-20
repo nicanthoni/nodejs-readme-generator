b9186a4c275db06eb4462617330f6fefbb3990b5
nic-d <nicanthonidiaz@gmail.com>
2023-10-20 15:55:02 -0400

    template of readme created; updated version of inquirer package and required it to index.js file. Creating questions to be asked by CLI

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Provide a project description. What is the 'why'?",
+    "How is the application installed?",
+    "How is the application used?",
+    "How may other developers contribute to this project?",
+    "How is the application tested? ",
+    "",
+    "",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +21,14 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
+// -- IMPORTANT -- 
+// Bc this application won’t be deployed, 
+// you’ll also need to provide a link to a walkthrough video that demonstrates its functionality.
+// Revisit 2.2.4: Screencastify Tutorial in Module 2 of the prework as a refresher on how to record video from your computer. 
+// You’ll need to submit a link to the video and add it to the README of your project.
