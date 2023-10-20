f05814fcb2a1667673eb93c5b9023e77d191e528
nic-d <nicanthonidiaz@gmail.com>
2023-10-20 16:16:48 -0400

    finished prompt command to store user questions as 'variables' to be used when generating readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    "What is the title of your project?",
-    "Provide a project description. What is the 'why'?",
-    "How is the application installed?",
-    "How is the application used?",
-    "How may other developers contribute to this project?",
-    "How is the application tested? ",
-    "",
-    "",
-];
+
+inquirer
+    .createPromptModule([
+        {
+            type: 'input',
+            name: 'project',
+            message: "What is the title of your project?"
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Provide a project description. What is the 'why'?"
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: "How is the application installed?"
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: "How is the application used?"
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: "How may other developers contribute to this project?"
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: "How is the application tested?"
+        }
+    ])
+
+ // takes answers from user input, uses as argugment in function that takes in name values (project,description,installation etc) and uses backticks and ${} to add to readme
+.then((answers)) => {
+    const readMeContent = generateReadMe(answers);
+}
+
+
+//  THIS IS WRONG, RIGHT? Decided on taking object array route above // 
+
+//          const questions = [
+// //     "What is the title of your project?",
+// //     "Provide a project description. What is the 'why'?",
+// //     "How is the application installed?",
+// //     "How is the application used?",
+// //     "How may other developers contribute to this project?",
+// //     "How is the application tested? ",
+// //     "",
+// // ];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
